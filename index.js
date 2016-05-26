858d530b96bb59e1ee2eb2b1939c1ef57558ad3d
vallettea <vallettea@gmail.com>
2016-05-26 11:25:16 +0200

    more data persisted

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 "use strict";
 
 var http = require('http');
+var fs = require('fs');
 
-var options = {
-    host: "data.bordeaux-metropole.fr",
-    path: '/wfs?key=G3TZNROCU3&service=wfs&version=1.1.0&request=GetFeature&typeName=CI_TRAFI_L&srsName=urn:ogc:def:crs:EPSG::4326',
-    method: 'GET'
-}
-var request = http.request(options, function (res) {
-    var data = '';
-    res.on('data', function (chunk) {
-        data += chunk;
-    });
-    res.on('end', function () {
-        console.log(data);
+var to_get = [
+    {"protocole": "wfs", "version": "1.1.0", "features": ["CI_TRAFI_L", "CI_PARK_P"], "options": '&srsName=urn:ogc:def:crs:EPSG::4326'},
+    {"protocole": "wps", "version": "1.0.0", "features": ["CI_TPSTJ_A"]}
+]
 
-    });
-});
-request.on('error', function (e) {
-    console.log(e.message);
-});
-request.end();
+to_get.forEach(function(obj){
+
+    obj.features.forEach(function(feature){
+
+        if (obj.protocole == "wfs") {
+
+            var options = {
+                host: "data.bordeaux-metropole.fr",
+                path: '/wfs?key=G3TZNROCU3&service=wfs&version=' + obj.version +'&request=GetFeature&typeName='+ feature + obj.options,
+                method: 'GET'
+            }
+        } else {
+
+            var options = {
+                host: "data.bordeaux-metropole.fr",
+                path: '/wps?key=G3TZNROCU3&service=wps&version=' + obj.version +'&request=Execute&identifier='+ feature,
+                method: 'GET'
+            }
+        }
+        var request = http.request(options, function (res) {
+            var data = '';
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+            res.on('end', function () {
+                var now = new Date();
+                var file_name = feature + "-" + now.toISOString() + '.xml';
+                fs.writeFile(file_name, data);
+            });
+        });
+        request.on('error', function (e) {
+            console.log(e.message);
+        });
+        request.end();
+
+    })
+
+
+
+})
+
