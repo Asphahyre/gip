67517f3b52a14675075c3b3dd68c1ada7a100373
vallettea <vallettea@gmail.com>
2016-05-26 11:27:53 +0200

    added env vars

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 var http = require('http');
 var fs = require('fs');
 
+var KEY = process.env.CUB_KEY;
+var DATA_PATH = process.env.CUB_DATA_PATH;
+
 var to_get = [
     {"protocole": "wfs", "version": "1.1.0", "features": ["CI_TRAFI_L", "CI_PARK_P"], "options": '&srsName=urn:ogc:def:crs:EPSG::4326'},
     {"protocole": "wps", "version": "1.0.0", "features": ["CI_TPSTJ_A"]}
@@ -16,14 +19,14 @@ to_get.forEach(function(obj){
 
             var options = {
                 host: "data.bordeaux-metropole.fr",
-                path: '/wfs?key=G3TZNROCU3&service=wfs&version=' + obj.version +'&request=GetFeature&typeName='+ feature + obj.options,
+                path: '/wfs?key=' + KEY + '&service=wfs&version=' + obj.version +'&request=GetFeature&typeName='+ feature + obj.options,
                 method: 'GET'
             }
         } else {
 
             var options = {
                 host: "data.bordeaux-metropole.fr",
-                path: '/wps?key=G3TZNROCU3&service=wps&version=' + obj.version +'&request=Execute&identifier='+ feature,
+                path: '/wps?key=' + KEY + '&service=wps&version=' + obj.version +'&request=Execute&identifier='+ feature,
                 method: 'GET'
             }
         }
@@ -35,7 +38,7 @@ to_get.forEach(function(obj){
             res.on('end', function () {
                 var now = new Date();
                 var file_name = feature + "-" + now.toISOString() + '.xml';
-                fs.writeFile(file_name, data);
+                fs.writeFile(DATA_PATH + file_name, data);
             });
         });
         request.on('error', function (e) {
